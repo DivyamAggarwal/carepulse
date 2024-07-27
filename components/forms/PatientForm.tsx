9f8f92fa476f50b35d76c1f5dcb9a6ef8b633df1
DivyamAggarwal <mailtodivyam1@gmail.com>
2024-07-28 00:25:51 +0530

    changes done

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -8,44 +8,44 @@ import { z } from "zod";
 import { Form } from "@/components/ui/form";
 import { createUser } from "@/lib/actions/patient.actions";
 import { UserFormValidation } from "@/lib/validation";
+
 import "react-phone-number-input/style.css";
 import CustomFormField, { FormFieldType } from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 
-  
- const PatientForm = () => {
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(false);
+const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const form = useForm<z.infer<typeof UserFormValidation>>({
-        resolver: zodResolver(UserFormValidation),
-        defaultValues: {
-          name: "",
-          email: "",
-          phone: "",
-        },
-      });
-    const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
-        setIsLoading(true);
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+      resolver: zodResolver(UserFormValidation),
+      defaultValues: {
+        name: "",
+        email: "",
+        phone: "",
+      },
+    });
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+      setIsLoading(true);
+   
+      try {
+        const user = {
+          name: values.name,
+          email: values.email,
+          phone: values.phone,
+        };
     
-        try {
-          const user = {
-            name: values.name,
-            email: values.email,
-            phone: values.phone,
-          };
+        const newUser = await createUser(user);
     
-          const newUser = await createUser(user);
+        if (newUser) {
+          router.push(`/patients/${newUser.$id}/register`);
+        }
+      } catch (error) {
+        console.log(error);
+        }
     
-          if (newUser) {
-            router.push(`/patients/${newUser.$id}/register`);
-          }
-        } catch (error) {
-          console.log(error);
-         }
-    
-        setIsLoading(false);
-      };      
+      setIsLoading(false);
+    };      
      
   return (
     <Form {...form}>
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,6 +1,6 @@
 "use server";
-
-import { ID, InputFile, Query } from "node-appwrite";
+//InputFile,
+import { ID,  Query } from "node-appwrite";
 import {
     BUCKET_ID,
     DATABASE_ID,
@@ -11,7 +11,8 @@ import {
     storage,
     users,
   } from "../appwrite.config";
-  export const createUser = async (user: CreateUserParams) => {
+import { parseStringify } from "../utils";
+export const createUser = async (user: CreateUserParams) => {
     try{
         const newuser = await users.create(
             ID.unique(),
@@ -20,6 +21,7 @@ import {
             undefined,
             user.name
         );
+        return parseStringify(newuser);
     }catch(error: any){
         if (error && error?.code === 409) {
             const existingUser = await users.list([
@@ -30,3 +32,53 @@ import {
     console.error("An error occurred while creating a new user:", error);
   }
 };
+export const getUser = async (userId: string) => {
+    try {
+      const user = await users.get(userId);
+  
+      return parseStringify(user);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the user details:",
+        error
+      );
+    }
+  };
+//   export const registerPatient = async ({
+//     identificationDocument,
+//     ...patient
+//   }: RegisterUserParams) => {
+//     try {
+//       // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+//       let file;
+//       if (identificationDocument) {
+//         const inputFile =
+//           identificationDocument &&
+//           InputFile.fromBlob(
+//             identificationDocument?.get("blobFile") as Blob,
+//             identificationDocument?.get("fileName") as string
+//           );
+  
+//         file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+//       }
+  
+//       // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+//       const newPatient = await databases.createDocument(
+//         DATABASE_ID!,
+//         PATIENT_COLLECTION_ID!,
+//         ID.unique(),
+//         {
+//           identificationDocumentId: file?.$id ? file.$id : null,
+//           identificationDocumentUrl: file?.$id
+//             ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+//             : null,
+//           ...patient,
+//         }
+//       );
+  
+//       return parseStringify(newPatient);
+//     } catch (error) {
+//       console.error("An error occurred while creating a new patient:", error);
+//     }
+//   };
+  
