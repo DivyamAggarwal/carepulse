caad9cdc2a66a491554988dbc009a44321edf702
DivyamAggarwal <mailtodivyam1@gmail.com>
2024-07-28 16:49:41 +0530

    admin front page done

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -13,43 +14,44 @@ import "react-phone-number-input/style.css";
 import CustomFormField, { FormFieldType } from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 
-const PatientForm = () => {
+export const PatientForm = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof UserFormValidation>>({
-      resolver: zodResolver(UserFormValidation),
-      defaultValues: {
-        name: "",
-        email: "",
-        phone: "",
-      },
-    });
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
   const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
-      setIsLoading(true);
-   
-      try {
-        const user = {
-          name: values.name,
-          email: values.email,
-          phone: values.phone,
-        };
-    
-        const newUser = await createUser(user);
-    
-        if (newUser) {
-          router.push(`/patients/${newUser.$id}/register`);
-        }
-      } catch (error) {
-        console.log(error);
-        }
-    
-      setIsLoading(false);
-    };      
-     
+    setIsLoading(true);
+
+    try {
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+
+      const newUser = await createUser(user);
+
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
     <Form {...form}>
-      <form  onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
           <p className="text-dark-700">Get started with appointments.</p>
@@ -86,7 +88,5 @@ const PatientForm = () => {
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
-  )
-}
-export default PatientForm;
-
+  );
+};
